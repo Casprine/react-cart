e66a18c8c657143be7f7f21ae4cc3fc8ad587117
Casprine Assempah <casprine.001@gmail.com>
2018-09-01 14:07:58 +0000

    styled the checkout button

--- src/components/CheckoutButton/Outlet.jsx
+++ src/components/CheckoutButton/Outlet.jsx
@@ -1,10 +1,13 @@
-import React, { Fragment } from "react";
+import React from "react";
 import "./style.css";
 const CheckoutButton = props => {
   return (
-    <Fragment>
-      <button> {props.totalPrice} </button>
-    </Fragment>
+    <div className="CheckoutButton">
+      <button className="checkout">
+        <span className="checkout-detail"> Checkout </span>
+        <span className="checkout-price">$ {props.totalPrice}</span>
+      </button>
+    </div>
   );
 };
 
