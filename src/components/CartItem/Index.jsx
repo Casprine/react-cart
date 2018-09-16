1bfe8b62ae693bd04c7e8d726227dd35fef6e53e
Casprine Assempah <casprine.001@gmail.com>
2018-09-16 06:46:43 +0000

    cartItem statically done

--- src/components/CartItem/Index.jsx
+++ src/components/CartItem/Index.jsx
@@ -1,11 +1,27 @@
 import React, { Fragment } from "react";
+import Button from "../Shared/Button";
+import "./style.css";
 
 const CartItem = props => {
-  return( 
-      <Fragment>
-          
-      </Fragment>
-  )
+  return (
+    <Fragment>
+      <div className="cart-item">
+        <img src={props.img} alt="" className="cart-img" />
+        <div className="item-details">
+          <div className="item-name">{props.name}</div>
+          <div className="item-decs">$ {props.price}</div>
+        </div>
+        <div className="counters">
+          <Button className="down" onClick={props.decrease} content="-" />
+          <div className="amount"> {props.quantity} </div>
+          <Button className="up" onClick={props.increase} content="+" />
+        </div>
+        <div className="price">$ {props.itemTotal}</div>
+        <div className="total">{props.total}</div>
+        <Button className="close" onClick={props.remove} content="x" />
+      </div>
+    </Fragment>
+  );
 };
 
 export default CartItem;
